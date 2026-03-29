b9f961ea631916b47ec53364ddcf3d3bdf4f0e17
antoinedmns <antoine.damiens1@orange.fr>
2026-03-29 19:27:24 +0200

    fix: convocationsSupp & convocationsPresents

--- src/routes/sessions/epreuves/convocations/postConvocationPresents.ts
+++ src/routes/sessions/epreuves/convocations/postConvocationPresents.ts
@@ -38,3 +38,64 @@ export async function postConvocationPresents(sessionId: string, epreuveCode: st
 
     return { success: epreuve.affectedRows > 0 };
 }
+
+/*
+export async function postConvocationPresents(sessionId: string, epreuveCode: string, presentsParSalle: Record<string, number>): Promise<APIBoolResponse> {
+
+    const idSession = parseInt(sessionId ?? '');
+
+    if (isNaN(idSession) || sessionId === undefined) {
+        throw new ErreurRequeteInvalide("L'ID de la session est invalide.");
+    }
+
+    if (!epreuveCode) {
+        throw new ErreurRequeteInvalide("Le code de l'epreuve est invalide.");
+    }
+
+    if (!presentsParSalle || !Array.isArray(presentsParSalle)) {
+        throw new ErreurRequeteInvalide("Le body doit être de la forme { salle: nbPresents }.");
+    }
+
+    const session = await sessionCache.getOrFetch(idSession);
+    if (!session) {
+        throw new ErreurRequeteInvalide("La session demandé n'existe pas.");
+    }
+
+    const epreuve = await session.epreuves.getOrFetch(epreuveCode);
+    if (!epreuve) {
+        throw new ErreurRequeteInvalide("L'épreuve demandé n'existe pas.");
+    }
+
+    const resultatsSalles = await Database.query<{ codeSalle: string }>("SELECT DISTINCT code_salle as codeSalle FROM convocation WHERE id_session = ? AND code_epreuve = ?;", [idSession, epreuveCode]);
+
+    const sallesConvocations = resultatsSalles.map(salle => salle.codeSalle);
+
+    let totalPresents = 0;
+
+    for (const salle of sallesConvocations) {
+        const nbPresents = presentsParSalle[salle];
+
+        if (nbPresents === undefined || nbPresents === null) {
+            throw new ErreurRequeteInvalide(`La salle ${salle} manque à la liste de présence des salles.`);
+        }
+
+        if (isNaN(nbPresents) || nbPresents < 0) {
+            throw new ErreurRequeteInvalide(`Le nombre de présents pour la salle ${salle} est invalide.`);
+        }
+
+        totalPresents += nbPresents;
+    }
+
+    const resultatsScans = await Database.query<{ compteurScans: number }>("SELECT COUNT(*) as compteurScans FROM convocation WHERE code_epreuve = ? AND id_session = ? AND note_quart IS NOT NULL;", [epreuveCode, idSession]);
+
+    const nbScans = resultatsScans[0]?.compteurScans ?? 0;
+
+    if (nbScans === totalPresents) {
+        // TODO: Changer le status (ex: epreuve.setStatus('TERMINE'))
+    }
+
+    const updateResult = await session.epreuves.update(epreuveCode, { nb_presents: totalPresents });
+
+    return { success: updateResult.affectedRows > 0 };
+}
+*/
